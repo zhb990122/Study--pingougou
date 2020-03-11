0da5238b48064af1bffde946c38e1053397c3c18
zhb990122 <409158229@qq.com>
2020-03-11 13:06:09 +0800

    第二次提交，增加看js轮播图

---
+++ js/index.js
@@ -0,0 +1,93 @@
+window.addEventListener('load', function () {
+    var arrow_l = this.document.querySelector('.arrow-l');
+    var arrow_r = this.document.querySelector('.arrow-r');
+    var focus = this.document.querySelector('.focus');
+    var focusWidth = focus.offsetWidth;
+    // 鼠标经过就显示，移走就不显示
+    focus.addEventListener('mouseenter', function () {
+        arrow_l.style.display = 'block';
+        arrow_r.style.display = 'block';
+        clearInterval(timer);
+        timer = null;
+    })
+    focus.addEventListener('mouseleave', function () {
+        arrow_l.style.display = 'none';
+        arrow_r.style.display = 'none';
+        timer = setInterval(function () {
+            arrow_r.click();
+        }, 3000);
+    })
+    // 动态生成小圆圈
+    var ul = focus.querySelector('ul');
+    var ol = focus.querySelector('.circle');
+    for (var i = 0; i < ul.children.length; i++) {
+        var li = document.createElement('li');
+        li.setAttribute('index', i);
+        ol.appendChild(li);
+        // 小圆圈的排他思想
+        li.addEventListener('click', function () {
+            for (var i = 0; i < ol.children.length; i++) {
+                ol.children[i].className = '';
+            }
+            this.className = 'current';
+            // 让小圆圈控制图片动画
+            var index = this.getAttribute('index');
+            num = circle = index;
+            animate(ul, -index * focusWidth);
+        })
+    }
+    ol.children[0].className = 'current';
+    var first = ul.children[0].cloneNode(true);
+    ul.appendChild(first);
+    // 点击按钮，图片滚动一张
+    var num = 0;
+    var circle = 0;
+    // 节流阀
+    var flag = true;
+    arrow_r.addEventListener('click', function () {
+        if (flag) {
+            flag = false;
+            if (num == ul.children.length - 1) {
+                ul.style.left = 0;
+                num = 0;
+            }
+            num++;
+            animate(ul, -num * focusWidth, function () {
+                flag = true;
+            });
+            // 点击右侧按钮，小圆圈一起变化
+            circle++;
+            if (circle == ol.children.length) {
+                circle = 0;
+            }
+            circleChange();
+        }
+    })
+    arrow_l.addEventListener('click', function () {
+        if (flag) {
+            flag = false;
+            if (num == 0) {
+                num = ul.children.length - 1;
+                ul.style.left = -num * focusWidth + 'px';
+            }
+            num--;
+            animate(ul, -num * focusWidth, function () {
+                flag = true;
+            });
+            // 点击右侧按钮，小圆圈一起变化
+            circle--;
+            circle = circle < 0 ? ol.children.length - 1 : circle;
+            circleChange();
+        }
+    })
+
+    function circleChange() {
+        for (var i = 0; i < ol.children.length; i++) {
+            ol.children[i].className = '';
+        }
+        ol.children[circle].className = 'current';
+    }
+    var timer = setInterval(function () {
+        arrow_r.click();
+    }, 3000);
+})
